d00063fe24fa893bf802bcca5c50925a530ac4e8
renzcabato1 <cabato.renz.renz@gmail.com>
2018-11-15 11:04:10 +0800

    User Approver Update

--- database/migrations/2018_11_15_030318_create_user_approvers_table.php
+++ database/migrations/2018_11_15_030318_create_user_approvers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UserApproversTable extends Migration
+class CreateUserApproversTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,6 @@ class UserApproversTable extends Migration
      */
     public function up()
     {
-        //
         Schema::create('user_approvers', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
@@ -29,6 +28,6 @@ class UserApproversTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('user_approvers');
     }
 }
